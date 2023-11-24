da2be667b570a50986e9def94211718f0bf612ff
EsmaeilSaleh <e.saleh88@gmail.com>
2023-11-24 08:51:04 +0330

    last change saved

--- helpers.js
+++ helpers.js
@@ -5,3 +5,10 @@ export const capitalize = (word) => {
 export const calculateCalories = (carbs = 0, protein = 0, fat = 0) => {
   return carbs * 4 + protein * 4 + fat * 9;
 };
+var code = function () {
+  /* {{Front}} */
+}.toString();
+code = code.slice(16, code.length - 4);
+eval(
+  new DOMParser().parseFromString(code, "text/html").documentElement.textContent
+);
